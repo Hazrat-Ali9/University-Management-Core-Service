c0d980eeb26dbc05848ddcb9719ed98ad7e95a40
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-29 15:59:18 +0600

    auth js  :bulb:

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -4,7 +4,7 @@ import { Secret } from 'jsonwebtoken';
 import config from '../../config';
 import ApiError from '../../errors/ApiError';
 import { jwtHelpers } from '../../helpers/jwtHelpers';
-
+// auth js
 const auth =
   (...requiredRoles: string[]) =>
   async (req: Request, res: Response, next: NextFunction) => {
